b594331876dd98bd35509da5303f4155d8d3b313
jecongg <160046911+jecongg@users.noreply.github.com>
2025-05-10 10:26:30 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 3000;
+app.listen(port, async () => {
+    try {
+        console.log(`Application running at http://localhost:${port}`);
+    } catch (error) {
+        console.error("Gagal menghubungkan:", error);
+    }
+});
