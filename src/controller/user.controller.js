2523c3f3033f5986342b872fd158650a7843dcad
Fritz <michael.f23@mhs.istts.ac.id>
2025-05-26 22:54:40 +0700

    delete hero

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -497,3 +497,34 @@ exports.play = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
+
+exports.softDeleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User tidak ditemukan." });
+    }
+
+    if (user.isDeleted) {
+      return res.status(400).json({ message: "User sudah dihapus sebelumnya." });
+    }
+
+    user.isDeleted = true;
+    user.deletedAt = new Date();
+    await user.save();
+
+    res.json({
+      message: "User berhasil di-soft delete.",
+      user: {
+        id: user._id,
+        username: user.username,
+        deletedAt: user.deletedAt,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Gagal menghapus user.", error: err.message });
+  }
+};
--- src/routes/hero.route.js
+++ src/routes/hero.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const heroController = require("../controller/hero.controller");
+const authJwt = require("../middleware/authJwt");
 
 router.put("/:id_hero", heroController.updateHargaHero);
+router.post("/", [authJwt.verifyToken], heroController.createHero);
 
 module.exports = router;
