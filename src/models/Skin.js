356a3ddb916d508b6ac9be3ac44863e88fe1b5ba
treviss11 <trevisartagrantdy@gmail.com>
2025-06-07 22:57:00 +0700

    .

--- src/models/Skin.js
+++ src/models/Skin.js
@@ -14,7 +14,8 @@ const skinSchema = new mongoose.Schema({
     isBuyable: {
         type: Boolean,
         default: true, // Default: bisa dibeli
-    }
+    },
+    image_skin: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Skin", skinSchema, "skins");
