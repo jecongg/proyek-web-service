dba43c8279520d3852d84d17639ff3a0c8ce57bb
jecongg <160046911+jecongg@users.noreply.github.com>
2025-06-08 15:18:28 +0700

    Topup and starlight payment done

--- src/routes/hero.route.js
+++ src/routes/hero.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const heroController = require("../controllers/hero.controller");
 const authJwt = require("../middleware/authJwt");
 
-router.put("/:id_hero", heroController.updateHargaHero);
+router.put("/:id_hero", [authJwt.verifyToken, authJwt.isAdmin], heroController.updateHargaHero);
 router.post("/", [authJwt.verifyToken, authJwt.isAdmin], heroController.createHero);
 router.get("/", heroController.getAllHeroes);
 
--- src/routes/skin.route.js
+++ src/routes/skin.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const skinController = require("../controllers/skin.controller");
+const authJwt = require("../middleware/authJwt");
 
 router.get("/", skinController.getAllSkins);
-router.put("/:id_skin", skinController.updateHargaSkin);
+router.put("/:id_skin", [authJwt.verifyToken, authJwt.isAdmin], skinController.updateHargaSkin);
 router.post("/:id_hero", [authJwt.verifyToken, authJwt.isAdmin], skinController.createSkinForHero);
 
 module.exports = router;
